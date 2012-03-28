d77038113ff274893864dc329a2dcfe42cb35737
Rob Hoelz <rob@hoelz.ro>
2012-03-28 19:19:33 +0200

    Use appropriate build flags on Lion

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -5,7 +5,9 @@ use Config;
 if ( $Config{myarchname} =~ /i386/ ) {
     my $os_version = qx(system_profiler SPSoftwareDataType);
     if($os_version =~ /System Version: Mac OS X (?:Server )?10\.(\d+)/) {
-        if($1 >= 5) { # Leopard and up
+        if($1 >= 7) { # Lion and up
+            $arch = "-arch x86_64 -arch i386 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.$1.sdk -mmacosx-version-min=10.$1";
+        } elsif($1 >= 5) { # Leopard and up
             $arch = "-arch x86_64 -arch i386 -isysroot /Developer/SDKs/MacOSX10.$1.sdk -mmacosx-version-min=10.$1";
         } else {
             $arch = "-arch i386 -arch ppc";
