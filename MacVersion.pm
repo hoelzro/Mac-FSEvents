0b398467ed1b92895432d9b29c9fe2a02b9e5359
Rob Hoelz <rob@hoelz.ro>
2012-07-30 13:41:29 +0200

    Fix version check for Mountian Lion

--- MacVersion.pm
+++ MacVersion.pm
@@ -9,7 +9,7 @@ our @EXPORT = qw(osx_version);
 
 sub osx_version {
     my $os_version = qx(system_profiler SPSoftwareDataType);
-    if($os_version =~ /System Version: Mac OS X (?:Server )?(10\.\d+)/) {
+    if($os_version =~ /System Version:(?: Mac)? OS X (?:Server )?(10\.\d+)/) {
         return $1;
     } else {
         $os_version =~ s/^/> /gm;
